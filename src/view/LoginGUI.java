c9738721ac11a3dab98c7058e437c403d55e581a
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2025-06-03 18:58:46 +0300

    logo done

--- src/view/LoginGUI.java
+++ src/view/LoginGUI.java
@@ -2,14 +2,16 @@ package view;
 
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
 
 public class LoginGUI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+	private JPanel w_pane;
 
 	/**
 	 * Launch the application.
@@ -31,12 +33,18 @@ public class LoginGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public LoginGUI() {
+		setResizable(false);
+		setTitle("Hastane Otomasyoonu");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setBounds(100, 100, 500, 450);
+		w_pane = new JPanel();
+		w_pane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
+		setContentPane(w_pane);
+		w_pane.setLayout(null);
+		
+		JLabel lbl_logo = new JLabel(new ImageIcon(getClass().getResource("medicine.png")));
+		lbl_logo.setBounds(196, 25, 100, 55);
+		w_pane.add(lbl_logo);
 	}
-
 }
